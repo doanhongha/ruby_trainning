043a54609575d067dec3ce366b4a81bb99b0344b
doanhongha <doanhong.ha@monstar-lab.com>
2017-11-21 13:59:59 +0900

    update migrations

--- ruby/ruby_trainning/db/schema.rb
+++ ruby/ruby_trainning/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171117074348) do
+ActiveRecord::Schema.define(version: 20171121045440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20171117074348) do
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -55,6 +56,8 @@ ActiveRecord::Schema.define(version: 20171117074348) do
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
+    t.string "avatar"
   end
 
 end
